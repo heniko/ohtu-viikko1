e6a262ba092230d2bc9fadb3370de22a1fa14546
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2018-11-01 12:35:44 +0200

    varastoTest rivikattavuus 100%

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -65,4 +65,85 @@ public class VarastoTest {
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
+    @Test
+    public void lisaaVarastoonAlleNolla() {
+        varasto.lisaaVarastoon(-10);
+
+        // varastossa pitäisi olla tilaa 10, sillä yritettiin vain lisätä negatiivinen määrä
+        assertEquals(10, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void lisaaVarastoonYliTilavuuden() {
+        varasto.lisaaVarastoon(20);
+
+        // Varastossa pitäisi olla tilaa 0, sillä sinne lisätään tavaraa enemmän kuin mahtuu
+        assertEquals(0, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void otaVarastostaAlleNolla() {
+        varasto.lisaaVarastoon(5);
+        varasto.otaVarastosta(-5);
+
+        // Täydestä varastosta yritetään ottaa negatiivinen määrä tavaraa
+        assertEquals(5, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void otaVarastostaEnemmanKuinVarastoSisaltaa() {
+        varasto.lisaaVarastoon(2);
+
+        // Varasto sisältää 2, mutta otetaan 3. Pitäisi palauttaa siis 2
+        assertEquals(2, varasto.otaVarastosta(3), vertailuTarkkuus);
+    }
+
+    @Test
+    public void toStringTest() {
+        varasto.lisaaVarastoon(2);
+        String testString = "saldo = 2.0, vielä tilaa 8.0";
+        
+        // toString() tulisi palauttaa "saldo = 2, vielä tilaa 8"
+        assertTrue(testString.equals(varasto.toString()));
+    }
+
+    @Test
+    public void varastoConstructor1() {
+        varasto = new Varasto(-1);
+
+        // Varaston tilavuuden tulisi olla 0
+        assertEquals(0, varasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void varastoConstructor2() {
+        varasto = new Varasto(-1, 0);
+
+        // Varaston tilavuuden tulisi olla 0
+        assertEquals(0, varasto.getTilavuus(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void varastoConstructor3() {
+        varasto = new Varasto(2, 3);
+
+        // Luodaan varasto ja lisätään sinne tavaraa yli kapasiteetin
+        assertEquals(2, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void varastoConstructor4() {
+        varasto = new Varasto(3, 2);
+
+        // Luodaan varasto tilavuudella 3 ja saldolla 2
+        assertEquals(2, varasto.getSaldo(), vertailuTarkkuus);
+    }
+
+    @Test
+    public void varastoConstructor5() {
+        varasto = new Varasto(2, -2);
+        
+        // Saldon tulisi olla 0
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
 }
